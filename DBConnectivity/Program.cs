a13f23d3896f284134fc0345316af83576af7207
Vigeshwaran <vigneshsobalamurugan2005@gmail.com>
2026-05-11 10:41:01 +0530

    Db COnnection

---
+++ DBConnectivity/Program.cs
@@ -0,0 +1,37 @@
+﻿using Npgsql;
+using System.Net.NetworkInformation;
+
+namespace UnderstandingADOApp
+{
+    
+    internal class Program
+    {
+        string connectionString =
+            "Host=localhost;Port=5432;Database=dummydb;Username=postgres;Password=978681";
+
+        public Program()
+        {
+            NpgsqlConnection connection = new NpgsqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Connected to PostgreSQL successfully!");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            new Program();
+
+        }
+    }
+}
---
+++ GameApp1/Models/Game.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+
+namespace GameApp.Models{
+    internal class Game{
+        public string HiddenWord { get; set; }
+        public int MaxAttempts { get; } = 6;
+        public int CurrAttempt { get; set; }
+        public List<string> PreviousGuesses { get; set;} = new List<string>();
+        public int score { get; set; }
+
+        public Game(string hiddenWord){
+            HiddenWord = hiddenWord;
+            CurrAttempt=0;
+            score = 0;
+        }
+
+        public bool IsCorrectGuess(string guessWord){
+            return guessWord.Equals(HiddenWord);
+        }
+
+        public void Reset(string hiddenWord){
+            HiddenWord = hiddenWord;
+            CurrAttempt = 0;
+            PreviousGuesses.Clear();
+            score = 0;
+        }
+
+        public bool IsGuessedAlready(string guessWord){
+            return PreviousGuesses.Contains(guessWord);
+        }
+
+        public int calculateScore(){
+            int baseScore = 100;
+            int penaltyPerAttempt = 15;
+            int score = baseScore - (CurrAttempt * penaltyPerAttempt);
+            return Math.Max(score, 0);
+        }
+
+        public void AddGuess(string guessWord){
+            PreviousGuesses.Add(guessWord);
+        }
+
+        public override string ToString(){
+            return $"----Word Game----\n Word: {HiddenWord} \n MaxAttempts: {MaxAttempts}\n CurrentAttempt: {CurrAttempt}\n PreviousGuesses: {string.Join(", ",PreviousGuesses)}\n ----Bye----";
+        }
+    }
+}
